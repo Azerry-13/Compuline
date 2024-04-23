3f4ad44a27706d324b4ed3805302904cbbe30221
Dmitrii <13azerry@gmail.com>
2024-04-23 19:25:13 +0600

    404 page

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,7 +5,31 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    const handleClick = (event) => {
+      const currentPath = window.location.pathname;
+      const linkPath = event.target.getAttribute('href');
+
+      if (currentPath === linkPath) {
+        event.preventDefault();
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      }
+    };
+
+    const links = document.querySelectorAll('li.small-text a');
+
+    links.forEach(link => {
+      link.addEventListener('click', handleClick);
+    });
+
+    // Удаляем обработчики событий при размонтировании компонента
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', handleClick);
+      });
+    };
   }, [pathname]);
 
   return null;
